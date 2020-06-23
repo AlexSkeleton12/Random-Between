c42e517410cd8ea7885696a93b355ca72de39fcc
AlexSkeleton12 <50224102+AlexSkeleton12@users.noreply.github.com>
2020-06-22 21:18:48 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startRange;
+            int endRange;
+            int midRange = 50;
+            int result = 0;
+
+            Console.Write("Chose the minimum number > ");
+            startRange = Int32.Parse(Console.ReadLine());
+
+            if (startRange != 0)
+            {
+                Console.WriteLine("I hate you... You didn't read the instructions!");
+                Thread.Sleep(1000);
+                Environment.Exit(0);
+            }
+
+            Console.Write("Chose the maximum number > ");
+            endRange = Int32.Parse(Console.ReadLine());
+
+            if (endRange != 100)
+            {
+                Console.WriteLine("I hate you... You didn't read the instructions!");
+                Thread.Sleep(1000);
+                Environment.Exit(0);
+            }
+
+            Console.Write("Chose the mid range number > ");
+            midRange = Int32.Parse(Console.ReadLine());
+
+            if (midRange <= startRange)
+            {
+                Console.WriteLine("I hate you... You didn't read the instructions!");
+                Thread.Sleep(1000);
+                Environment.Exit(0);
+            }
+
+            if (midRange >= endRange)
+            {
+                Console.WriteLine("I hate you... You didn't read the instructions!");
+                Thread.Sleep(1000);
+                Environment.Exit(0);
+            }
+
+            Random r = new Random();
+            int rInt = r.Next(startRange, endRange);
+
+            if (rInt > midRange)
+            {
+                result = 2;
+            }
+
+            if (rInt < midRange)
+            {
+                result = 1;
+            }
+
+            Console.WriteLine(rInt.ToString());
+            Console.WriteLine(result.ToString());
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Random Between
  A simple C# app...
+
+# Instructions:
+Just run the exe file
+
+# Limits:
+The middle range cant be the same or not in between the start range and end range
+
+# changelog:
+1.0 - Release June 22, 2020
